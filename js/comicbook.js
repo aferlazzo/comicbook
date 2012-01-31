fb13c43f139fa30fdf6293e1eb7ae9648fb2f9f7
Anthony Ferlazzo <aferlazzo@gmail.com>
2012-01-31 07:35:30 -0800

    first update

---
+++ js/comicbook.js
@@ -0,0 +1,26 @@
+  // handy trick for preloaing images
+new Image().src = "img/comicbook-sprite.png";
+
+  (function () {  // self-executing anonymous function for copyright notice
+      var Today = new Date();
+      $('#thisYear').text(Today.getFullYear());
+  })();
+  
+  var i;
+  
+  spinHead = function(){
+	$('#alfred-head').css({'-moz-transform': 'rotate(' + i + 'deg)',
+					 '-webkit-transform': 'rotate(' + i + 'deg)'});  	
+  	i += 10;
+  	if(i < 361){
+  		setTimeout('spinHead()', 5);
+	}
+  }
+  
+  
+  doSpin = function(){
+  	i=10;
+  	spinHead();
+  }
+  
+ setTimeout('doSpin()', 7000); 
