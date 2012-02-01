6dca4ab83e5d56dad4aff7d0be1d9e2a32addcbd
Anthony Ferlazzo <aferlazzo@gmail.com>
2012-01-31 19:23:25 -0800

    01/31/12 update

--- js/comicbook.js
+++ js/comicbook.js
@@ -1,5 +1,21 @@
   // handy trick for preloaing images
 new Image().src = "img/comicbook-sprite.png";
+new Image().src = "img/0.jpg";
+new Image().src = "img/1.jpg";
+new Image().src = "img/2.jpg";
+new Image().src = "img/3.jpg";
+new Image().src = "img/4.jpg";
+new Image().src = "img/5.jpg";
+new Image().src = "img/6.jpg";
+new Image().src = "img/7.jpg";
+new Image().src = "img/8.jpg";
+new Image().src = "img/9.jpg";
+new Image().src = "img/10.jpg";
+new Image().src = "img/11.jpg";
+new Image().src = "img/12.jpg";
+new Image().src = "img/13.jpg";
+new Image().src = "img/14.jpg";
+new Image().src = "img/15.jpg";
 
   (function () {  // self-executing anonymous function for copyright notice
       var Today = new Date();
@@ -21,6 +37,7 @@ new Image().src = "img/comicbook-sprite.png";
   doSpin = function(){
   	i=10;
   	spinHead();
+  	setTimeout('doSpin()', 7000);
   }
   
  setTimeout('doSpin()', 7000); 
