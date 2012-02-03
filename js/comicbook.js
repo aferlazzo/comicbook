0e7c0d54a1b23cc8a90bd7e79d935a1dcd28a47d
Anthony Ferlazzo <aferlazzo@gmail.com>
2012-02-03 14:45:41 -0800

    improved alfred picture and clean graphics

--- js/comicbook.js
+++ js/comicbook.js
@@ -1,27 +1,4 @@
-// handy trick for preloaing images
-new Image().src = "img/comicbook-sprite.png";
-new Image().src = "img/0.jpg";
-new Image().src = "img/1.jpg";
-new Image().src = "img/2.jpg";
-new Image().src = "img/3.jpg";
-new Image().src = "img/4.jpg";
-new Image().src = "img/5.jpg";
-new Image().src = "img/6.jpg";
-new Image().src = "img/7.jpg";
-new Image().src = "img/8.jpg";
-new Image().src = "img/9.jpg";
-new Image().src = "img/10.jpg";
-new Image().src = "img/11.jpg";
-new Image().src = "img/12.jpg";
-new Image().src = "img/13.jpg";
-new Image().src = "img/14.jpg";
-new Image().src = "img/15.jpg";
-new Image().src = "img/pronto.png";
-new Image().src = "img/lightning.png";
-new Image().src = "img/clipper.png";
-new Image().src = "img/mapmarkers.png";
-new Image().src = "img/perfect-response.png";
-new Image().src = "img/batman-background.jpg";
+
 
 (function () {  // self-executing anonymous function for copyright notice
       var Today = new Date();
@@ -35,9 +12,9 @@ var i, pictureNo = 0;
 spinHead = function (){
 	$('#alfred-head').css({'-moz-transform': 'rotate(' + i + 'deg)',
 					 '-webkit-transform': 'rotate(' + i + 'deg)'});
-  	i += 10;
+  	i += 20;
   	if(i < 361){
-  		setTimeout('spinHead()', 5);
+  		setTimeout('spinHead()', 3);
 	}
 };
 
@@ -88,8 +65,6 @@ setTimeout('changePicture()', 5000);
 $('document').ready(function (){
 	var i, portfolioTimer = null;
 	
-	$('.contrib:eq(0)').css('display', 'block');	
-	
 	$('td, a').click(function (){
 		var index = $(this).index();
 		clearTimeout(portfolioTimer);
@@ -134,6 +109,7 @@ $('document').ready(function (){
 		doIt();
 	})();
 
+	$('#cover, .contrib:eq(0)').css('display', 'block');	
 
 });
 
